f1534f92d28f304aa3d9a7f08908cec5153bf990
Starlord0alpha <notgoatmichael@gmail.com>
2022-07-13 19:22:27 +0800

    Create2 Delegate2Call SelfDestruct modify

--- security/SelfDestruct.sol
+++ security/SelfDestruct.sol
@@ -4,11 +4,31 @@ pragma solidity ^0.8.0;
 /*
     The selfdestruct(address) function removes all bytecode from the contract address and sends all ether stored to the specified address.
     If this specified address is also a contract, no functions (including the fallback) get called.
+
+    1. delete contract
+    2. force send Ether to any address
 */
 
-contract SelfDestruct {
+contract Kill {
 
-    function SelfDestruct(){
+    constructor() payable {}
 
+    function kill() external {
+        selfdestruct(payable(msg.sender));
+    }
+
+    function testCall() external pure returns (uint) {
+        return 123;
+    }
+}
+
+contract Helper {
+
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+
+    function kill(Kill _kill) external {
+        _kill.kill();
     }
 }
