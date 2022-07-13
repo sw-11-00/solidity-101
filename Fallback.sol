7a7a17729a5328c082df30a7dbef9b8985714f00
Starlord0alpha <848074915@qq.com>
2022-07-13 09:24:30 +0800

    Fallback.sol

---
+++ Fallback.sol
@@ -0,0 +1,37 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+// fallback function
+
+// function doesn't exist
+// directly send eth
+
+/*
+  fallback() or receive() ?
+      Ether is sent to contract
+            is msg.data empty ?
+                 /         \
+                yes        no
+               /            \
+      receive() exists?   fallback()
+             /      \
+           yes      no
+           /         \
+      receive()   fallback()
+
+      receive函数只负责接受主币
+*/
+contract Fallback {
+
+    event Log(string func, address sender, uint value, bytes data);
+
+    fallback() external payable {
+        emit Log("fallback", msg.sender, msg.value, msg.data);
+    }
+
+    receive() external payable {
+        emit Log("receive", msg.sender, msg.value, "");
+    }
+
+
+}
