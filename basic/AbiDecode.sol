9ca71820e352816a6bf001681d3efc228d55ace4
Starlord0alpha <notgoatmichael@gmail.com>
2022-07-15 19:19:21 +0800

    abi decode multicall

---
+++ basic/AbiDecode.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract AbiDecode {
+
+    struct MyStruct {
+        string name;
+        uint[2] nums;
+    }
+
+    function encode(uint x,
+        address addr,
+        uint[] calldata arr,
+        MyStruct calldata myStruct
+    ) external pure returns (bytes memory) {
+        return abi.encode(x, addr, arr, myStruct);
+    }
+
+    function decode(bytes calldata data) external pure returns (uint x, address addr, uint[] memory arr, MyStruct memory myStruct) {
+        (x, addr, arr, myStruct) = abi.decode(data, (uint, address, uint[], MyStruct));
+    }
+}
