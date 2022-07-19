ea696c082675f1bae9ba61b7c6519cd817692055
Starlord0alpha <notgoatmichael@gmail.com>
2022-07-20 00:03:27 +0800

    Add interview related

---
+++ contracts/Defi/Compound/Compound.js
@@ -0,0 +1,7 @@
+// 1. Compound利率模型
+// https://medium.com/steaker-com/defi-%E7%9A%84%E4%B8%96%E7%95%8C-compound-%E5%AE%8C%E5%85%A8%E8%A7%A3%E6%9E%90-%E5%88%A9%E7%8E%87%E6%A8%A1%E5%9E%8B%E7%AF%87-95e9b303c284
+// https://zhuanlan.zhihu.com/p/126503548
+
+// 2. borrowIndex的计算
+
+// 3. 
---
+++ contracts/Defi/Curve/Curve.js
@@ -0,0 +1,5 @@
+// 1. Curve Finance的自动化做市商（AMM）原理
+//     https://zhuanlan.zhihu.com/p/461398413
+//     curve主要的功能是交易稳定币，
+// 2. Curve Convex的关系
+// 3. 贿选
