a0da2bf1f6ab161b1e38bda2ef6848dc66b590c0
Starlord0alpha <notgoatmichael@gmail.com>
2022-07-21 08:13:48 +0800

    restructure Add interview record and organize notes function

--- contracts/interview/miscellaneous.js
+++ contracts/interview/miscellaneous.js
@@ -4,3 +4,10 @@
 // 3. EIP721A
 // 4. 生成钱包地址，前面几位是固定的，如何生成，例如说0x88888，后面是随机的
 // 5. keystore是如何存储private key的
+// 6. safeerc20跟erc20的区别，是有返回值
+// 7. EVM的亮点(栈、堆，gas优化)
+// 8. assembly指令集
+// 9. EVM层面deploy contract和execute contract过程(google -> deconstruct solidity)
+// 10. keccak256涉及到的native contract合约
+// 11. go-ethereum
+// 12.
